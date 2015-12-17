2a4677a33080c276f3b89652569546055e8aeff2
helpfulrobot <chris@silverstripe.com>
2015-12-18 10:53:39 +1300

    Converted to PSR-2

--- code/Forum_Subscribers.php
+++ code/Forum_Subscribers.php
@@ -1,9 +1,10 @@
 <?php
 //
-class Forum_Subscribers extends DataObject {
+class Forum_Subscribers extends DataObject
+{
 
     private static $db = array(
-    'Type' => 'varchar(20)',    
+    'Type' => 'varchar(20)',
     );
 
     private static $has_one = array(
@@ -11,23 +12,25 @@ class Forum_Subscribers extends DataObject {
     'Member' => 'Member'
     );
 
-	/**
-	 * Checks to see if a Member is already subscribed to this thread
-	 *
-	 * @param int $forumID The ID of the forum to check
-	 * @param int $memberID The ID of the currently logged in member (Defaults to Member::currentUserID())
-	 * @return bool true if they are subscribed, false if they're not
-	 */
-	static function already_subscribed($forumID, $memberID = null) {
-		if(!$memberID) $memberID = Member::currentUserID();
-		$SQL_forumID = Convert::raw2sql($forumID);
-		$SQL_memberID = Convert::raw2sql($memberID);
- 		
-		if(DB::query("SELECT ID FROM Forum_Subscribers WHERE `ForumID` = '$SQL_forumID' AND `MemberID` = '$SQL_memberID'")->value()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+    /**
+     * Checks to see if a Member is already subscribed to this thread
+     *
+     * @param int $forumID The ID of the forum to check
+     * @param int $memberID The ID of the currently logged in member (Defaults to Member::currentUserID())
+     * @return bool true if they are subscribed, false if they're not
+     */
+    public static function already_subscribed($forumID, $memberID = null)
+    {
+        if (!$memberID) {
+            $memberID = Member::currentUserID();
+        }
+        $SQL_forumID = Convert::raw2sql($forumID);
+        $SQL_memberID = Convert::raw2sql($memberID);
+        
+        if (DB::query("SELECT ID FROM Forum_Subscribers WHERE `ForumID` = '$SQL_forumID' AND `MemberID` = '$SQL_memberID'")->value()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- code/SubscribeExtension.php
+++ code/SubscribeExtension.php
@@ -5,46 +5,45 @@
  * @package forum_subscribe
  * 
  */
-class SubscribeExtension extends DataExtension {
+class SubscribeExtension extends DataExtension
+{
 
-	
-	private static $many_many = array(
-		'Subscribers' => 'Member'		
-	);
-	
-	/**
-	  * print subscribe/unsubscribe link
-	 */
-	public function SubscribeLink() {	
-		//Requirements::css('forum_subscribe/css/style.css');		
-		$interface = new SSViewer('ForumSubscribe');		
-		
-		$controller = new SubscribeController();
-		
-		
-		$loggedIn = (Member::currentUser())? true : false;
-		
-		$back_url = isset($_SERVER['REQUEST_URI'])?$_SERVER['REQUEST_URI']:false;
-		
-		return $interface->process(new ArrayData(array(
-			'LoggedIn' 			=> $loggedIn,
-			'Subscribed' 			=> $this->IsSubscribed(),
-			'Parent'			=> $this->owner,						
-			'SubscribeLink' 			=> $controller->Link('subscribe/'.$this->owner->ID),
-			'UnSubscribeLink' 			=> $controller->Link('unsubscribe/'.$this->owner->ID),
-			'Settings'			=> $controller->Link('settings').'?RedirectURL='.urlencode($back_url),
-		)));
-		
-	}
-	
-	public function IsSubscribed(){
-	
-		$forum_id = $this->owner->ID;		
-		$member_id = Member::currentUserID();
-		
-		return Forum_Subscribers::already_subscribed($forum_id,$member_id);		
-	}
-	
-	
+    
+    private static $many_many = array(
+        'Subscribers' => 'Member'
+    );
+    
+    /**
+      * print subscribe/unsubscribe link
+     */
+    public function SubscribeLink()
+    {
+        //Requirements::css('forum_subscribe/css/style.css');		
+        $interface = new SSViewer('ForumSubscribe');
+        
+        $controller = new SubscribeController();
+        
+        
+        $loggedIn = (Member::currentUser())? true : false;
+        
+        $back_url = isset($_SERVER['REQUEST_URI'])?$_SERVER['REQUEST_URI']:false;
+        
+        return $interface->process(new ArrayData(array(
+            'LoggedIn'            => $loggedIn,
+            'Subscribed'            => $this->IsSubscribed(),
+            'Parent'            => $this->owner,
+            'SubscribeLink'            => $controller->Link('subscribe/'.$this->owner->ID),
+            'UnSubscribeLink'            => $controller->Link('unsubscribe/'.$this->owner->ID),
+            'Settings'            => $controller->Link('settings').'?RedirectURL='.urlencode($back_url),
+        )));
+    }
+    
+    public function IsSubscribed()
+    {
+        $forum_id = $this->owner->ID;
+        $member_id = Member::currentUserID();
+        
+        return Forum_Subscribers::already_subscribed($forum_id, $member_id);
+    }
 }//
 
